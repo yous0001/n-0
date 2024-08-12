7d29031c7f3ebc5d8e1e8eff3ec358654458a1b0
Yousef emad mikhail <mad33833@gmail.com>
2024-08-12 15:36:17 +0300

    fix: fix navbar style and logo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 function changeNavBG(){
     let scrollValue=window.scrollY
-    if(scrollValue<=100){
+    if(scrollValue<=10){
         document.getElementById("nav").classList.remove("nav-bg")
         document.getElementById("links").classList.remove("links-bg-white")
+        document.getElementById("logo").src="./imgs/logo-light.png"
+        document.getElementById("nav").style.height="80px"
     }
     else{
     document.getElementById("nav").classList.add("nav-bg")
     document.getElementById("links").classList.add("links-bg-white")
+    document.getElementById("logo").src="./imgs/logo-dark.png"
+    document.getElementById("nav").style.height="60px"
 }
 }
 window.addEventListener('scroll',changeNavBG)
